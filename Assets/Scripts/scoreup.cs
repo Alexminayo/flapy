7e141900d109643c22ffcf2216f4e862151cf09a
alexminayo <alexminayo90@gmail.com>
2014-10-27 17:27:29 +0100

    flapy version beta

--- Assets/Scripts/scoreup.cs
+++ Assets/Scripts/scoreup.cs
@@ -4,14 +4,18 @@ using UnityEngine.UI;
 public class scoreup : MonoBehaviour {
 
 	private Text texto;
+	private Text textoFinal;
+
 
 	void Start(){
 				texto = GameObject.Find ("textoNivel").GetComponent<Text> ();
+		textoFinal = GameObject.Find ("textoFinal").GetComponent<Text> ();
 		}
 	void OnTriggerEnter2D(Collider2D col){
 		GameControl.score = GameControl.score + 1;
 		Debug.Log (GameControl.score);
 
 			texto .text = "Level " + GameControl.score.ToString ();
+		textoFinal.text = texto.text;
 	}
 }
